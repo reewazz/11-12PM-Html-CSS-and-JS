462d4c7a9a144607c8388cb32adeea84c94373ed
Riktriz <riwaj.neupane15@gmail.com>
2026-06-28 12:21:07 +0545

    dom practice

---
+++ javascript/dom.js
@@ -0,0 +1,32 @@
+// const logoh1 = document.getElementById("logo")
+
+//   {
+//     color : coral
+      //  font-size : 14px
+//   }
+
+// fontSize
+// backgroundColor
+// borderRadius
+
+// logoh1.style.color = 'red'
+
+// console.log(logoh1)
+
+const navid = document.getElementById('navid')
+navid.style.backgroundColor = 'black'
+navid.style.color = 'white'
+// navid.style.color = 'red'
+
+
+const product = document.getElementById("prod")
+product.style.color = 'green'
+// console.log(navid)
+
+
+const btn = document.getElementById("btn")
+btn.style.backgroundColor = "coral"
+btn.style.color = "white"
+btn.style.fontSize = "12px"
+btn.style.fontWeight = "600"
+btn.style.padding = "10px"
